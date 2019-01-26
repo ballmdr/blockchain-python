030ccc5162088a413d16f198adb3d7393c1cb7e3
Ballmdr <ball.mdr@gmail.com>
2019-01-26 11:56:38 +0700

    FXCM

--- zipline/first.py
+++ zipline/first.py
@@ -1,19 +1,31 @@
-from zipline.api import order, record, symbol
-
-# %pylab inline
-# figsize(12,12)
-# import matplotlib.pyplot as pyplot
-# ax1 = plt.subplot(211)
-# perf.portfolio_value.plot(ax=ax1)
-# ax1.setylabel('Portfolio Value')
-# ax2 = plt.subplot(212, sharex=ax1)
-# perf.AAPL.plot(ax=ax2)
-# ax2.set_ylabel('AAPL Stock Price')
+from zipline.api import order_target, record, symbol
 
 def initialize(context):
-    pass
+    context.i = 0
+    context.asset = symbol('AAPL')
+
 
 def handle_data(context, data):
-    order(symbol('AAPL'), 10)
-    record(AAPL=data.current(symbol('AAPL'), 'price'))
+    # Skip first 300 days to get full windows
+    context.i += 1
+    if context.i < 300:
+        return
 
+    # Compute averages
+    # data.history() has to be called with the same params
+    # from above and returns a pandas dataframe.
+    short_mavg = data.history(context.asset, 'price', bar_count=100, frequency="1d").mean()
+    long_mavg = data.history(context.asset, 'price', bar_count=300, frequency="1d").mean()
+
+    # Trading logic
+    if short_mavg > long_mavg:
+        # order_target orders as many shares as needed to
+        # achieve the desired number of shares.
+        order_target(context.asset, 100)
+    elif short_mavg < long_mavg:
+        order_target(context.asset, 0)
+
+    # Save values for later inspection
+    record(AAPL=data.current(context.asset, 'price'),
+           short_mavg=short_mavg,
+           long_mavg=long_mavg)
