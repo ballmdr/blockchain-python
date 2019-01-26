a91ffae6fa61ece8ffede18e4b4991fea9f90624
Ballmdr <ball.mdr@gmail.com>
2019-01-26 21:17:55 +0700

    zipline test

--- zipline/first.py
+++ zipline/first.py
@@ -1,31 +1,78 @@
-from zipline.api import order_target, record, symbol
+from zipline.api import order, record, symbol, order_target
+from zipline.algorithm import TradingAlgorithm
+import pytz
+from datetime import datetime
+import pandas as pd
+
 
 def initialize(context):
-    context.i = 0
-    context.asset = symbol('AAPL')
-
+    context.security = symbol('AAPL')
 
 def handle_data(context, data):
-    # Skip first 300 days to get full windows
-    context.i += 1
-    if context.i < 300:
-        return
+    MA1 = data[context.security].mavg(50)
+    MA2 = data[context.security].mavg(100)
+    date = str(data[context.security].datetime)[:10]
+    current_price = data[context.security].price
+    current_positions = context.portfolio.positions[symbol('AAPL')].amount
+    cash = context.portfolio.cash
+    value = context.portfolio.portfolio_value
+    current_pnl = context.portfolio.pnl
 
-    # Compute averages
-    # data.history() has to be called with the same params
-    # from above and returns a pandas dataframe.
-    short_mavg = data.history(context.asset, 'price', bar_count=100, frequency="1d").mean()
-    long_mavg = data.history(context.asset, 'price', bar_count=300, frequency="1d").mean()
+    if (MA1 > MA2) and current_positions == 0:
+        number_of_shares = int(cash/current_price)
+        order(context.security, number_of_shares)
+        record(date=date,MA1 = MA1, MA2 = MA2, Price= 
+    current_price,status="buy",shares=number_of_shares,PnL=current_pnl,cash=cash,value=value)
 
-    # Trading logic
-    if short_mavg > long_mavg:
-        # order_target orders as many shares as needed to
-        # achieve the desired number of shares.
-        order_target(context.asset, 100)
-    elif short_mavg < long_mavg:
-        order_target(context.asset, 0)
+    elif (MA1 < MA2) and current_positions != 0:
+        order_target(context.security, 0)
+        record(date=date,MA1 = MA1, MA2 = MA2, Price= current_price,status="sell",shares="--",PnL=current_pnl,cash=cash,value=value)
 
-    # Save values for later inspection
-    record(AAPL=data.current(context.asset, 'price'),
-           short_mavg=short_mavg,
-           long_mavg=long_mavg)
+    else:
+        record(date=date,MA1 = MA1, MA2 = MA2, Price= current_price,status="--",shares="--",PnL=current_pnl,cash=cash,value=value)
+
+def getDateRange(df='', fromdate='', todate=''):
+
+    mask = (df['Datetime'] > fromdate) & (df['Datetime'] <= todate)
+    return df.loc[mask]
+
+def getPandaCsv(ori_path='', symbol='', filename='', fromdate='', todate=''):
+
+    path = ori_path + '/' + symbol + filename + '.csv'
+    
+    df = pd.read_csv(path, names=['date', 'time', 'Open', 'High', 'Low', 'Close', 'Volume'])
+    df['Datetime'] = pd.to_datetime(df['date'] + ' ' + df['time'])
+
+    #print(df['date'].tail())
+    
+    #print(df.loc[mask].tail())
+
+    #df.index = df['Date']
+
+    df.drop(['date', 'time'], axis=1, inplace=True)
+
+    if fromdate != '' and todate != '':
+        df = getDateRange(df=df, fromdate=fromdate, todate=todate)
+
+    df.index = df['Datetime']
+    del df['Datetime']
+
+    return df
+
+
+if __name__ == "__main__":
+    
+    ori_path = '/Users/ballmdr/Documents/data'
+    symbol = 'EURUSD'
+    filename = '_M1_2012_2019'
+    fromdate = datetime(2012,1,1)
+    todate = datetime(2012,12,13)
+
+    data = getPandaCsv(ori_path=ori_path, symbol=symbol, filename=filename, fromdate=fromdate, todate=todate)
+
+    #data.to_csv('/Users/ballmdr/Documents/data/test.csv')
+
+    algo_obj = TradingAlgorithm(initialize = initialize, handle_data = handle_data, caital_base = 1000.0)
+
+    perf_manual = algo_obj.run(data)
+    perf_manual[["MA1","MA2","Price"]].plot()
